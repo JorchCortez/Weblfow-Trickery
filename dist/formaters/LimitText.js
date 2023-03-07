438a30fea27879248c04f071a578723f7cfeaa37
Joorch <jacortez_94@hotmail.com>
2023-03-06 21:31:08 -0600

    Updated limit text order of text length attributes.

--- dist/formaters/LimitText.js
+++ dist/formaters/LimitText.js
@@ -19,7 +19,7 @@
 let LimitText = () => {
 
     let cardsText = document.querySelectorAll('[wf-util-limittext-item="text"]');
-    let textLength = 300 || timeDisplay.getAttribute("wf-util-limittext-length");
+    let textLength = timeDisplay.getAttribute("wf-util-limittext-length") || 300 ;
     console.log(textLength)
     for (text of cardsText) {
         var Description = text.innerText;
