a6d4ed85aa43aa0961afe1d2df4d901a808946b0
Joorch <jacortez_94@hotmail.com>
2023-03-07 11:47:11 -0600

    Update LimitText.js

--- dist/formaters/LimitText.js
+++ dist/formaters/LimitText.js
@@ -19,8 +19,8 @@
 let LimitText = () => {
 
     let cardsText = document.querySelectorAll('[wf-util-limittext-item="text"]');
-    let textLength = cardsText.getAttribute("wf-util-limittext-length") || 300 ;
     for (text of cardsText) {
+        let textLength = text.getAttribute("wf-util-limittext-length") || 300 ;
         var Description = text.innerText;
         if (Description.length > textLength) {
             var shortDesc = Description.substring(0, textLength) + "...";
