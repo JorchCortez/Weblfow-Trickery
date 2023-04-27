f39c4d68b21a93e1b732caca472b0872a232f38f
Joorch <jacortez_94@hotmail.com>
2023-04-27 13:50:14 -0600

    [Hotfix] Fixing social link to encoded version

--- dist/utilities/SocialLinks.js
+++ dist/utilities/SocialLinks.js
@@ -29,39 +29,42 @@ const SetSocialShare = () => {
     if( social_fb.length === 0 && social_tw.length === 0 && social_ln.length === 0 && social_wp.length === 0 && social_pi.length === 0 &&
         social_red.length === 0 && social_cp.length === 0 ) return;
 
+    let unencodedURI = url + '&title=' + title + '&description=' + title 
+    let encoded = encodeURI(unencodedURI);
+    
     if (social_fb) {
         social_fb.forEach((social_btn) => {
-            social_btn.setAttribute("href", 'https://www.facebook.com/sharer/sharer.php?u=' + url + '&title=' + title );
+            social_btn.setAttribute("href", 'https://www.facebook.com/sharer/sharer.php?u=' + encoded );
             social_btn.setAttribute("target", '_blank');
         });
     }
     if (social_tw) {
         social_tw.forEach((social_btn) => {  
-            social_btn.setAttribute("href", 'https://twitter.com/share?url=' + url + '&title=' + title );
+            social_btn.setAttribute("href", 'https://twitter.com/share?url=' + encoded );
             social_btn.setAttribute("target", '_blank');
         });
     }
     if (social_ln) {
         social_ln.forEach((social_btn) => {
-            social_btn.setAttribute("href", 'https://www.linkedin.com/shareArticle?mini=true&url=' + url + '&title=' + title );
+            social_btn.setAttribute("href", 'https://www.linkedin.com/shareArticle?mini=true&url=' + encoded );
             social_btn.setAttribute("target", '_blank');
         });
     }
     if (social_wp) {
         social_wp.forEach((social_btn) => {
-            social_btn.setAttribute("href", 'https://wa.me/?text=' + url + '&title=' + title );
+            social_btn.setAttribute("href", 'https://wa.me/?text=' + encoded );
             social_btn.setAttribute("target", '_blank');
         });
     }
     if (social_pi) {
         social_pi.forEach((social_btn) => {
-            social_btn.setAttribute("href", 'http://pinterest.com/pin/create/button/?url=' + url + '&description=' + title );
+            social_btn.setAttribute("href", 'http://pinterest.com/pin/create/button/?url=' + encoded );
             social_btn.setAttribute("target", '_blank');
         });
     }
     if (social_red) {
         social_red.forEach((social_btn) => {
-            social_btn.setAttribute("href", 'http://www.reddit.com/submit?url=' + url + '&title=' + title );
+            social_btn.setAttribute("href", 'http://www.reddit.com/submit?url=' + encoded );
             social_btn.setAttribute("target", '_blank');
         });
     } 
