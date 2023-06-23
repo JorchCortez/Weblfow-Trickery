117e6c696703a331065ff2c9273e20cea5f49071
Joorch <jacortez_94@hotmail.com>
2023-06-23 14:21:55 -0700

    [Feature] Added B&A slider code

---
+++ dist/components/befoe-after.js
@@ -0,0 +1,27 @@
+/*!
+ * Webflow Utilities v1.0.10
+ * Have you ever tried fixing the same small thing over and over again on your webflow projects?
+ * Well I have so I made a small compillation of the different fixes, utilities and tricks that will
+ * not only make my life easier in the future but hopefuly yours as well.
+ * (c) 2023 Jorge Cortez
+ * MIT License
+ * https://github.com/JorchCortez/Weblfow-Trickery
+ */
+
+const InitializeBASliders = () => {
+    const BASliders = document.querySelectorAll('[wt-bna-element="slider"]');
+    BASliders.forEach(slider => { 
+        let sliderWrapper = slider.parentElement;
+      slider.addEventListener('input', (e) => {
+          sliderWrapper.style.setProperty('--position', `${e.target.value}%`)
+      })
+    })
+}
+
+if (/complete|interactive|loaded/.test(document.readyState)) {
+    InitializeBASliders();
+} else { 
+    window.addEventListener('DOMContentLoaded', function () { 
+        InitializeBASliders();
+    })
+}
