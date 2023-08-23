d3c5a1744cec3b4729c395a09e834affb15abd86
Jorch C <jacortez_94@hotmail.com>
2023-08-23 14:41:44 -0700

    Update RangeSlider.js

--- dist/utilities/RangeSlider.js
+++ dist/utilities/RangeSlider.js
@@ -8,7 +8,7 @@
  * https://github.com/JorchCortez/Weblfow-Trickery
  */
 
-const setupRangeSliders = (inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd) => {
+const setupRangeSliders = ({inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd, sliderMin, sliderMax, sliderSteps}) => {
 
     let triggerEvent = (elem) =>{
         elem.dispatchEvent(new Event('input', { bubbles: true }));
@@ -69,17 +69,32 @@ const setupRangeSliders = (inputLeft, inputRight, thumbLeft, thumbRight, range, 
     inputRight.addEventListener("input", setRightValue);
 }
 
-const setupInputBoxes = (inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd) => {
+const setupInputBoxes = ({inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd, sliderMin, sliderMax, sliderSteps}) => {
 
     let setLeftValue = () => {
-        if(!rangeStart || rangeStart.value === null) {
+        if(rangeStart.value === null || rangeStart.value === "") {
             console.log("start val got cleared")
+
+            if(displayStart){
+                if(displayStart) displayStart.innerHTML = sliderMin;
+            }
+            inputLeft.value = sliderMin;
+            thumbLeft.style.left = "0%";
+            range.style.left = "0%";
         }
     }
 
     let setRightValue = () => {
-        if(!rangeEnd || rangeEnd.value === null) {
-            console.log("end val got cleared")
+        if(rangeEnd.value === null || rangeEnd.value === "") {
+            console.log("start val got cleared")
+
+            if(displayStart){
+                if(displayEnd) displayEnd.innerHTML = sliderMax;
+            }
+            
+            inputRight.value = sliderMax;
+            thumbRight.style.right = "0%";
+            range.style.right = "0%";
         }
     }
 
@@ -99,9 +114,9 @@ const initializeRangeSlider = () => {
 
     rangeSliders.forEach((rangeSlider) => {
         //get input values
-        let sliderMin       = rangeSlider.querySelector('[wt-rangeslider-min="input-left"]');
-        let sliderMax       = rangeSlider.querySelector('[wt-rangeslider-max="input-left"]');
-        let sliderSteps     = rangeSlider.querySelector('[wt-rangeslider-steps="input-left"]');
+        let sliderMin       = rangeSlider.getAttribute('wt-rangeslider-min');
+        let sliderMax       = rangeSlider.getAttribute('wt-rangeslider-max');
+        let sliderSteps     = rangeSlider.getAttribute('wt-rangeslider-steps');
 
         //getting range range slider's index if multiple
         let _triggerAttr = rangeSlider.getAttribute(`wt-rangeslider-element`);
@@ -121,21 +136,34 @@ const initializeRangeSlider = () => {
         let range           = rangeSlider.querySelector('[wt-rangeslider-element="range"]');
 
 
-        if(!inputLeft || !inputRight || !thumbLeft || !thumbRight || !range) {
+        if(!inputLeft || !inputRight || !thumbLeft || !thumbRight || !range || !sliderMin || !sliderMax ) {
             console.log("%cThanks for using Webflow Trickery!","color: red; font-size: 18px; padding: 8px");            
             console.log("%cIt seems like you're missing an element that is required for the integration.","color: red; font-size: 12px; padding: 8px");
             checkElement(inputLeft, "Input-left");
             checkElement(inputRight, "Input-right");
             checkElement(thumbLeft, "Thumb-left");
             checkElement(thumbRight, "Thumb-right");
+            checkElement(thumbLeft, "Thumb-left");
+            checkElement(thumbRight, "Thumb-right");
+            checkElement(sliderMin, "Slider-Min");
+            checkElement(sliderMax, "Slider-Max");
             checkElement(range, "Range");
             console.log("%cPlease keep in mind this elements must be insde of your container with the 'wt-rangeslider-element=slider' element","color: red; font-size: 12px; padding: 8px");
             return;
         }
         
-        setupRangeSliders(inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd);
-        if(rangeStart || rangeEnd) {
-            setupInputBoxes(inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd);
+        inputLeft.setAttribute("min", sliderMin);
+        inputLeft.setAttribute("max", sliderMax);
+        inputLeft.setAttribute("step", sliderSteps);
+        inputRight.setAttribute("min", sliderMin);
+        inputRight.setAttribute("max", sliderMax);
+        inputRight.setAttribute("step", sliderSteps);
+        
+        let sliderData = {inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd, sliderMin, sliderMax, sliderSteps}
+        
+        setupRangeSliders(sliderData);
+        if(rangeStart && rangeEnd) {
+            setupInputBoxes(sliderData);
         }
     })
 }
