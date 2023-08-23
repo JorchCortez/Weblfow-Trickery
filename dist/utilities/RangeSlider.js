45e09b8bfaaf5f9d982e49e4c10cf78b54d55245
Jorch C <jacortez_94@hotmail.com>
2023-08-23 11:22:31 -0700

    Update RangeSlider.js

--- dist/utilities/RangeSlider.js
+++ dist/utilities/RangeSlider.js
@@ -8,13 +8,22 @@
  * https://github.com/JorchCortez/Weblfow-Trickery
  */
 
-const setupRangeSlider =(inputLeft, inputRight, thumbLeft, thumbRight, range) => {
+const setupRangeSliders = (inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd) => {
+
     let setLeftValue = () => {
         var _this = inputLeft,
             min = parseInt(_this.min),
             max = parseInt(_this.max);
 
-        _this.value = Math.min(parseInt(_this.value),parseInt(inputRight.value) - 1 );
+        if(rangeStart === displayStart){
+            rangeStart.value = inputLeft.value;
+        }
+        else {
+            if(rangeStart) rangeStart.value = inputLeft.value;
+            if(displayStart) displayStart.innerHTML = inputLeft.value;
+        }
+            
+        _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
 
         var percent = ((_this.value - min) / (max - min)) * 100;
         let posFix = parseInt(percent) / 11;
@@ -27,11 +36,19 @@ const setupRangeSlider =(inputLeft, inputRight, thumbLeft, thumbRight, range) =>
             min = parseInt(_this.min),
             max = parseInt(_this.max);
 
-        _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1 );
+        if(rangeStart === displayEnd){
+            if(rangeEnd) rangeEnd.value = inputRight.value;
+        }
+        else {
+            if(rangeEnd) rangeEnd.value = inputRight.value;
+            if(displayEnd) displayEnd.innerHTML = inputRight.value;
+        }
+
+        _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
 
         var percent = ((_this.value - min) / (max - min)) * 100;
-        
-        let posFix = parseInt(percent/10) - 10;
+
+        let posFix = parseInt(percent / 10) - 10;
         thumbRight.style.right = (100 - parseInt(percent) + parseInt(posFix)) + "%";
         range.style.right = (100 - parseInt(percent)) + "%";
     }
@@ -43,23 +60,55 @@ const setupRangeSlider =(inputLeft, inputRight, thumbLeft, thumbRight, range) =>
     inputRight.addEventListener("input", setRightValue);
 }
 
+const checkElement = (element, name) => {
+    if (!element) {
+        console.log(`%c${name} element is missing`,"color: red; font-size: 12px; padding: 8px");
+    }
+};
+
 const initializeRangeSlider = () => {
-	const rangeSliders = document.querySelectorAll('[wt-rangeslider-element="slider"]');
-	
-	rangeSliders.forEach((rangeSlider) => {
-		let inputLeft = rangeSlider.querySelector('[wt-rangeslider-element="input-left"]');
-		let inputRight = rangeSlider.querySelector('[wt-rangeslider-element="input-right"]');
-		let thumbLeft = rangeSlider.querySelector('[wt-rangeslider-element="thumb-left"]');
-		let thumbRight = rangeSlider.querySelector('[wt-rangeslider-element="thumb-right"]');
-		let range = rangeSlider.querySelector('[wt-rangeslider-element="range"]');
-        setupRangeSlider(inputLeft, inputRight, thumbLeft, thumbRight, range)
-	})
+    const rangeSliders = document.querySelectorAll('[wt-rangeslider-element^="slider-"], [wt-rangeslider-element="slider"]');
+    if(!rangeSliders) return;
+
+    rangeSliders.forEach((rangeSlider) => {
+        //getting range range slider's index if multiple
+        let _triggerAttr = rangeSlider.getAttribute(`wt-rangeslider-element`);
+        let index = _triggerAttr.replace('slider', '')
+
+        //get displays and inputs if different.
+        let rangeStart      = document.querySelector(`[wt-rangeslider-range="from${index}"]`)
+        let rangeEnd        = document.querySelector(`[wt-rangeslider-range="to${index}"]`)
+        let displayStart    = document.querySelector(`[wt-rangeslider-display="from${index}"]`)
+        let displayEnd      = document.querySelector(`[wt-rangeslider-display="to${index}"]`)
+
+        //getting required range slider elements
+        let inputLeft       = rangeSlider.querySelector('[wt-rangeslider-element="input-left"]');
+        let inputRight      = rangeSlider.querySelector('[wt-rangeslider-element="input-right"]');
+        let thumbLeft       = rangeSlider.querySelector('[wt-rangeslider-element="thumb-left"]');
+        let thumbRight      = rangeSlider.querySelector('[wt-rangeslider-element="thumb-right"]');
+        let range           = rangeSlider.querySelector('[wt-rangeslider-element="range"]');
+
+
+        if(!inputLeft || !inputRight || !thumbLeft || !thumbRight || !range) {
+            console.log("%cThanks for using Webflow Trickery!","color: red; font-size: 18px; padding: 8px");            
+            console.log("%cIt seems like you're missing an element that is required for the integration.","color: red; font-size: 12px; padding: 8px");
+            checkElement(inputLeft, "Input-left");
+            checkElement(inputRight, "Input-right");
+            checkElement(thumbLeft, "Thumb-left");
+            checkElement(thumbRight, "Thumb-right");
+            checkElement(range, "Range");
+            console.log("%cPlease keep in mind this elements must be insde of your container with the 'wt-rangeslider-element=slider' element","color: red; font-size: 12px; padding: 8px");
+            return;
+        }
+        
+        setupRangeSliders(inputLeft, inputRight, thumbLeft, thumbRight, range, rangeStart, rangeEnd, displayStart, displayEnd)
+    })
 }
 
 if (/complete|interactive|loaded/.test(document.readyState)) {
     initializeRangeSlider();
-} else { 
-    window.addEventListener('DOMContentLoaded', function () { 
+} else {
+    window.addEventListener('DOMContentLoaded', function () {
         initializeRangeSlider();
     })
 }
